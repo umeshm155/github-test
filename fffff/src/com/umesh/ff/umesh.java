c8b003f36ddd62435b075152d7111db930c40420
mohit <umeshm155@gmail.com>
2022-04-19 13:52:37 +0530

    v3 commit by mohit

--- fffff/src/com/umesh/ff/umesh.java
+++ fffff/src/com/umesh/ff/umesh.java
@@ -1,10 +1,10 @@
 package com.umesh.ff;
 
-public class test {
+public class umesh {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("umesh mahajan");
+		System.out.println("mahajan");
 	}
 
 }
